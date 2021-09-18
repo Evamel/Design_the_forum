179aa6806511b2f0303e7776bdf2331b9f51e8ef
Eva Melinand <eva.melinand@gmail.com>
2021-09-18 11:25:49 +0200

    Copy design The Forum

---
+++ src/app/views/includes/footer.php
@@ -0,0 +1,30 @@
+<footer>
+            <div class="logos">
+                <ul>
+                <li><i class="fab fa-twitter"></i></li>
+                <li><i class="fab fa-apple"></i></li>
+                <li><i class="fab fa-facebook-f"></i></li>
+                <li><i class="fab fa-codepen"></i></li>
+                <li><i class="fab fa-google-plus-g"></i></li>
+                <li><i class="fab fa-digg"></i></li>
+                <li><i class="fab fa-pinterest-p"></i></li>
+                </ul>
+            </div>
+            <div class="lastLine">
+            <ul class="endPagePath">
+                <li><a href="#"><i class="fas fa-home"></i>  Home</a></li>
+                <li><a href="#">
+                    <  Boards</a></li>
+            </ul>
+            <ul class="endPageLinks">
+                <li><a href="#"><i class="fas fa-envelope"></i> Contact us</a></li>
+                <li><a href="#"><i class="fas fa-shield-alt"></i> The team</a></li>
+                <li><a href="#"><i class="fas fa-check"></i> Terms</a></li>
+                <li> <a href="#"><i class="fas fa-lock"></i> Privacy</a></li>
+                <li> <a href="#"><i class="fas fa-users"></i> Members</a></li>
+                <li><a href="#"><i class="fas fa-trash-alt"></i> Delete cookies</a></li>
+                <li>All time are UTC</li>
+            </ul>
+            <a class="btn-up" href="#top"><i class="fas fa-arrow-up"></i></a>
+        </div>
+        </footer>
---
+++ src/app/views/includes/header.php
@@ -0,0 +1,22 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a7577a9f0e.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="/src/public/css/bootstrap.min.css">  
+    <link rel="stylesheet" href="/src/public/css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300,400,500,600,800&display=swap" rel="stylesheet">
+    <title>Topic</title>
+</head>
+<body>
+    <header>
+        <div class="log">
+            <a href="#"><i class="far fa-edit"></i> Register</a>
+            <a href="#"><i class="fas fa-sign-in-alt"></i> Login</a>
+        </div>
+
+<h1>The forum</h1>
+
+    </header>
